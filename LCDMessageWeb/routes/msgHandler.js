17d06fbe49301c0327efae5c1bd07a8367cbbf55
Herrick Spencer <herrick.spencer@microsoft.com>
2022-03-30 16:23:58 -0700

    send something with serial

--- LCDMessageWeb/routes/msgHandler.js
+++ LCDMessageWeb/routes/msgHandler.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
 
 router.post('/msgHandle', function (req, res) {
     console.log(req.body);
+
+    // send msg to cigarbox (serial port)
+    // https://cmsdk.com/node-js/how-to-write-in-serial-port-using-nodejs.html
+    var sp = new SerialPort( {
+        path: "COM3",
+        baudRate: 9600
+      });
+      sp.on('open', function () 
+      {
+          console.log("writing...");
+          sp.write(req.body.msg);
+          console.log("done");
+      });
+      
     res.render('index', { title: 'Cigar Box Message handled', msg: req.body.msg });
   });
   
