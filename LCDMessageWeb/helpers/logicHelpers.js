c6da95f6a0bcfba6a61e92b1fc5ae8f087e602e6
Herrick Spencer <herrick.spencer@microsoft.com>
2022-04-01 13:29:50 -0700

    look for started

---
+++ LCDMessageWeb/helpers/logicHelpers.js
@@ -0,0 +1,29 @@
+//**********************************************************************
+// function waitfor - Wait until a condition is met
+//        
+// Needed parameters:
+//    test: function that returns a value
+//    expectedValue: the value of the test function we are waiting for
+//    msec: delay between the calls to test
+//    callback: function to execute when the condition is met
+// Parameters for debugging:
+//    count: used to count the loops
+//    source: a string to specify an ID, a message, etc
+//**********************************************************************
+var wait = function (test, expectedValue, msec, count, source, callback){
+    // Check if condition met. If not, re-check later (msec).
+    while (test() !== expectedValue) {
+        count++;
+        setTimeout(function() {
+            wait(test, expectedValue, msec, count, source, callback);
+        }, msec);
+        return;
+    }
+    // Condition finally met. callback() can be executed.
+    console.log(source + ': ' + test() + ', expected: ' + expectedValue + ', ' + count + ' loops.');
+    callback();    
+};
+
+module.exports = {
+    waitFor : wait
+};
--- LCDMessageWeb/routes/msgHandler.js
+++ LCDMessageWeb/routes/msgHandler.js
@@ -1,28 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var messageHelper = require("../helpers/messageHelper");
+var logicHelpers = require("../helpers/logicHelpers");
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
+var lines = {};
+var started = false;
+var startedBuf = "";
+var sp;
 
 router.post('/msgHandle', function (req, res) {
     console.log(req.body);
 
-    var lines = messageHelper.FormatMessage(req.body.msg);
+    lines = messageHelper.FormatMessage(req.body.msg);    
 
     // send msg to cigarbox (serial port)
     // https://cmsdk.com/node-js/how-to-write-in-serial-port-using-nodejs.html
-    var sp = new SerialPort( {
+    sp = new SerialPort( {
         path: "COM3",
         baudRate: 9600
       });
-      sp.on('open', function () 
-      {
-          console.log("writing...");
-          sp.write(req.body.msg);
-          console.log("done");
-      });
+    // Switches the port into "flowing mode"
+    sp.on('data', function (data) {
+        console.log('Serial Data:', data.toString("utf8"))
+        startedBuf =startedBuf.concat(startedBuf, data.toString("utf8"));
+        if (startedBuf.indexOf("STARTED") >= 0)
+        {
+            started = true;
+        }
+    });
+
+    sp.on('open', function () 
+    {    
+        logicHelpers.waitFor(()=>{return started},true,50,0,'msgHandle->sp.open', ()=>{
+            console.log("writing...");
+            lineNbr = 0;
+            var rtn = Buffer.from("13","hex");
+            var ack = Buffer.from("06","hex");
+
+            lines.map(msg=>{
+                var ln = Buffer.from(lineNbr.toString());            
+                var data = Buffer.from(lines[lineNbr++]);
+                var buf = Buffer.concat([ack, ln, data, rtn]);
+                sp.write(buf);
+                console.log("done with line " + lineNbr.toString() + buf.toString('utf8'));
+            });
+            console.log("done sending messages");
+            var bufprt = Buffer.concat([ack, Buffer.from("5,PRINTSCR"), rtn]);
+            sp.write(bufprt);
+            console.log("PRINT " + bufprt.toString('utf8'));
+        });
+    });
       
     res.render('index', { title: 'Cigar Box Message handled', msg: req.body.msg });
-  });
+});
   
 module.exports = router;
